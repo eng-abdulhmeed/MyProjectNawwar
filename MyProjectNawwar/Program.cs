eaa62701058f6dd123b22434bdb960596c5e220f
tariq <tariq@TARIQSWEAR>
2026-08-09 23:08:59 +0300

    j

--- MyProjectNawwar/Program.cs
+++ MyProjectNawwar/Program.cs
@@ -16,7 +16,7 @@ namespace MyProjectNawwar
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new Log_in());
         }
     }
 }
